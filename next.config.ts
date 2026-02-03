78ad0623915bda9db1de5e956cfd22cc49832232
JordanTreDaniel <lilj390150@live.com>
2026-02-03 16:03:23 -0600

    Change to support custom domain - jordanchristley.com

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/jordanchristley-site",
   trailingSlash: true,
   images: {
     unoptimized: true,
